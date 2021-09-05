79602c69358aee6f95b061a5ca8028f2a527df60
Viki Val <viktorko99@gmail.com>
2021-09-05 20:24:37 +0200

    Another pack of unused components

--- src/components/shared/format/Percent.vue
+++ src/components/shared/format/Percent.vue
@@ -7,7 +7,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 
 @Component
-export default class Money extends Vue {
+export default class Percent extends Vue {
   @Prop({default: 0}) readonly value: number | string | undefined;
 
   get chainProperties() {
